8529f99e80d0bfd035ceee9a81b113c10e89f899
Davi Cavalcante <davicc@outlook.com.br>
2021-12-08 19:07:47 -0300

    print-println

--- Eclipse/2021/curso_programacao/src/Main.java
+++ Eclipse/2021/curso_programacao/src/Main.java
@@ -3,8 +3,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Olá mundo!");
-
+		int y = 32;
+		double x = 10.35784;
+		System.out.println(y);
+		System.out.println(x);
+		System.out.print("Olá mundo!");
+        System.out.println("Bom dia!");
 	}
 
 }
