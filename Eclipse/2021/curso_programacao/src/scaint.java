ee5377d0a12abfefb766a4e47ee1166ad08c42bf
Davi Cavalcante <davicc@outlook.com.br>
2021-12-09 00:01:27 -0300

    scan

---
+++ Eclipse/2021/curso_programacao/src/scaint.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class scaint {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x;
+		x = sc.nextInt();
+		System.out.println("Você digitou: " + x);
+		
+		sc.close();
+	}
+
+}
