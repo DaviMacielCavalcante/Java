a9fd82645601a7882371b783555b87b9b91272d1
Davi Cavalcante <davicc@outlook.com.br>
2021-12-08 22:27:47 -0300

    uso de variaveis

--- Eclipse/2021/curso_programacao/src/Main.java
+++ Eclipse/2021/curso_programacao/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 
 public class Main {
 
@@ -5,11 +6,20 @@ public class Main {
 		
 		int y = 32;
 		double x = 10.35784;
+		String nome = "Maria";
+		int idade = 31;
+		double renda = 4000.0;
 		System.out.println(y);
 		System.out.println(x);
 		System.out.printf("%.2f%n", x);
+		System.out.printf("%.4f%n", x);
 		System.out.print("Olá mundo!");
         System.out.println("Bom dia!");
+        Locale.setDefault(Locale.US);
+        System.out.printf("%.4f%n", x);
+        System.out.println("RESULTADO = " + x + " METROS");
+        System.out.printf("RESULTADO = %.2f metros%n", x);
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
 	}
 
 }
