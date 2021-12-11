71c0743735f263f6d871e9826abed0fa58c52ca3
Davi Cavalcante <davicc@outlook.com.br>
2021-12-11 20:13:53 -0300

    debugteste

---
+++ Eclipse/2021/curso_programacao/src/estruturas_de_repetição/debugteste.java
@@ -0,0 +1,33 @@
+package estruturas_de_repetição;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class debugteste {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);		
+		double L, C, A, Preco, PrecoMetro;		
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Informe a largura do terreno: ");
+		L = input.nextDouble();
+		System.out.println("Informe o comprimento do terreno: ");
+		C = input.nextDouble();
+		System.out.println("Informe o preço do metro quadrado: ");
+		Preco = input.nextDouble();
+		
+		A = L * C;
+		PrecoMetro = Preco * A;
+		
+		System.out.printf("AREA = %.2f%n", A);
+		System.out.printf("PREÇO = %.2f%n", PrecoMetro);
+		
+		
+
+		input.close();
+	}
+}
+
+
