a14e25826c27087171294f53fcf6d1faa18fee63
Davi Cavalcante <davicc@outlook.com.br>
2022-04-16 15:51:33 -0300

    seção do JavaFX finalizada

---
+++ Eclipse/Curso_Java_Udemy/javafx5/src/gui/ViewController.java
@@ -0,0 +1,51 @@
+package gui;
+
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import gui.util.Alerts;
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+
+public class ViewController implements Initializable {
+
+	@FXML
+	private TextField txtNumber1;
+	
+	@FXML
+	private TextField txtNumber2;
+	
+	@FXML
+	private Label labelResult;
+	
+	@FXML
+	private Button btSum;
+	
+	@FXML
+	public void onBtSumAction() {
+		try {
+			Locale.setDefault(Locale.US);
+			double n1 = Double.parseDouble(txtNumber1.getText());
+			double n2 = Double.parseDouble(txtNumber2.getText());
+			double sum = n1 + n2;
+			labelResult.setText(String.format("%.2f", sum));
+		}
+		catch(NumberFormatException e) {
+			Alerts.showAlert("Error", "Parse error", e.getMessage(), AlertType.ERROR);
+		}
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		Constraints.setTextFieldDouble(txtNumber1);
+		Constraints.setTextFieldDouble(txtNumber2);
+		Constraints.setTextFieldMaxLength(txtNumber1, 12);
+		Constraints.setTextFieldMaxLength(txtNumber2, 12);
+	}
+}
