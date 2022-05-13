6a2e11603272e76166511a908b1ba324ae5cfd42
Davi Cavalcante <davicc@outlook.com.br>
2022-05-13 20:03:27 -0300

    Service layer, component registration

---
+++ ws-sts/web_services_with_spring_boot_and_Hibernate/src/main/java/learning/java/course/services/UserService.java
@@ -0,0 +1,26 @@
+package learning.java.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import learning.java.course.entities.User;
+import learning.java.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
