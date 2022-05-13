51981d429b62a10a9ef2c95dd8ed158483888668
Davi Cavalcante <davicc@outlook.com.br>
2022-05-13 08:29:45 -0300

    Project Created

---
+++ ws-sts/web_services_with_spring_boot_and_Hibernate/src/main/java/learning/java/course/WebServicesWithSpringBootAndHibernateApplication.java
@@ -0,0 +1,13 @@
+package learning.java.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesWithSpringBootAndHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesWithSpringBootAndHibernateApplication.class, args);
+	}
+
+}
---
+++ ws-sts/web_services_with_spring_boot_and_Hibernate/src/test/java/learning/java/course/WebServicesWithSpringBootAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.java.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesWithSpringBootAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
