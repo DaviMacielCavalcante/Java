2f8792e8648604f831649e8267f935a1d6f7f141
Davi Cavalcante <davicc@outlook.com.br>
2022-05-13 09:28:51 -0300

    User entity and resource

---
+++ ws-sts/web_services_with_spring_boot_and_Hibernate/src/main/java/learning/java/course/resources/UserResources.java
@@ -0,0 +1,20 @@
+package learning.java.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import learning.java.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
